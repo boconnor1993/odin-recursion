9c7b06699fa19cd85c648260b5532098f6759b8c
boconnor1993 <brendan_oconnor1993@hotmail.com>
2024-10-13 21:58:14 +1000

    Added merge sort

---
+++ sort.js
@@ -0,0 +1,27 @@
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(leftArr, rightArr) {
+    const sortedArr = [];
+
+    while (leftArr.length && rightArr.length) {
+        if (leftArr[0] <= rightArr[0]) {
+            sortedArr.push(leftArr.shift());
+        } else {
+            sortedArr.push(rightArr.shift());
+        }
+    }
+
+    return sortedArr.concat(leftArr).concat(rightArr);
+}
+
+console.log(mergeSort([8, 20, 5, 18, 0, 6]));
